3b8205b41c249b48bec8d9890a08c492e797378d
Phatlynx <kevawo@gmail.com>
2021-09-30 15:33:41 -0500

    second commit

---
+++ arrays-ds.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+std::vector<int> reverseArray(std::vector<int> a)
+{
+    int aux = 0;
+    int n = a.size();
+    for (int i = 0; i < n / 2; ++i)
+    {
+        aux = a[n - i - 1];
+        a[n - i - 1] = a[i];
+        a[i] = aux;
+    }
+    return a;
+}
+
+int main()
+{
+    std::vector<int> array = {2, 9, 11, 20, 25};
+
+    array = reverseArray(array);
+    for (auto i : array)
+        std::cout << i << std::endl;
+}
